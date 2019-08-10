679a6e2e03a1cb8651c8a2a70356b4063a487364
Pavel <chelsea2048@mail.ru>
2019-08-10 23:50:16 +0300

    write project

--- app/src/main/java/com/example/money_tracker/MainActivity.java
+++ app/src/main/java/com/example/money_tracker/MainActivity.java
@@ -1,77 +1,61 @@
 package com.example.money_tracker;
 
-import android.content.Context;
-import android.support.annotation.LayoutRes;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-    public class Item {
+    static class Item {
         String name;
         int price;
 
-        public Item(String name, int price) {
+        Item(String name, int price) {
             this.name = name;
-            this.price = price;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public void setPrice(int price) {
             this.price = price;
         }
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.main);
         final EditText name = (EditText) findViewById(R.id.name);
         final EditText price = (EditText) findViewById(R.id.price);
         final Button add = (Button) findViewById(R.id.add);
-        final ListView items = findViewById(R.id.items);
+        final ListView items = (ListView) findViewById(R.id.items);
+        final ItemsAdapter adapter = new ItemsAdapter();
 
-        //items.setAdapter(new ItemsAdapter());
-
+        items.setAdapter(adapter);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             new Item(name.getText().toString(), Integer.valueOf(price.getText().toString()));
+                adapter.add(new Item(name.getText().toString(), Integer.valueOf(price.getText().toString())));
             }
         });
     }
 
-
-
-    class ItemsAdapter extends ArrayAdapter<Item> {
-        public ItemsAdapter(@NonNull Context context, @LayoutRes int resource) {
-            super(context, R.layout.item);
+    private class ItemsAdapter extends ArrayAdapter<Item> {
+        ItemsAdapter() {
+            super(MainActivity.this, R.layout.item);
         }
 
-      /*  @NonNull
+        @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @androidx.annotation.NonNull ViewGroup parent) {
-            getLayoutInflater().inflate(R.layout.item, parent);
-        }*/
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            final View view = getLayoutInflater().inflate(R.layout.item, null);
+            final Item item = getItem(position);
+            ((TextView) view.findViewById(R.id.name)).setText(item.name);
+            ((TextView) view.findViewById(R.id.price)).setText(String.valueOf(item.price));
+            return view;
+        }
     }
 }
